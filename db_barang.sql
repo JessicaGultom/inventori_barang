6a62ace7dbbc54d7b56ad505997ca4be7cbab528
sasdindah <79888314+sasdindah@users.noreply.github.com>
2021-06-10 00:31:11 +0700

    membuat file database baru

---
+++ db_barang.sql
@@ -0,0 +1,342 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: 09 Jun 2021 pada 18.21
+-- Versi Server: 10.1.19-MariaDB
+-- PHP Version: 5.6.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_barang`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `barang`
+--
+
+CREATE TABLE `barang` (
+  `id_barang` char(7) NOT NULL,
+  `nama_barang` varchar(255) NOT NULL,
+  `stok` int(11) NOT NULL,
+  `satuan_id` int(11) NOT NULL,
+  `jenis_id` int(11) NOT NULL,
+  `qrcode` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data untuk tabel `barang`
+--
+
+INSERT INTO `barang` (`id_barang`, `nama_barang`, `stok`, `satuan_id`, `jenis_id`, `qrcode`) VALUES
+('B000000', 'Tiga Roda', 5, 2, 10, 'B000000.png'),
+('B000001', 'Nippon Paint', 10, 6, 11, 'B000001.png'),
+('B000002', 'Sanyo', 18, 8, 12, 'B000002.png');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `barang_keluar`
+--
+
+CREATE TABLE `barang_keluar` (
+  `id_barang_keluar` char(16) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `barang_id` char(7) NOT NULL,
+  `jumlah_keluar` int(11) NOT NULL,
+  `tanggal_keluar` date NOT NULL,
+  `qrcode` varchar(50) NOT NULL,
+  `status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data untuk tabel `barang_keluar`
+--
+
+INSERT INTO `barang_keluar` (`id_barang_keluar`, `user_id`, `barang_id`, `jumlah_keluar`, `tanggal_keluar`, `qrcode`, `status`) VALUES
+('T-BK-21060900000', 1, 'B000002', 2, '2021-06-09', '', 0),
+('T-BK-21060900001', 1, 'B000001', 1, '2021-06-09', '', 0);
+
+--
+-- Trigger `barang_keluar`
+--
+DELIMITER $$
+CREATE TRIGGER `update_stok_keluar` AFTER UPDATE ON `barang_keluar` FOR EACH ROW BEGIN
+      IF (NEW.status = 1) THEN
+      	UPDATE `barang` SET `barang`.`stok` = `barang`.`stok` - NEW.jumlah_keluar WHERE `barang`.`id_barang` = NEW.barang_id;
+      END IF;
+    END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `barang_masuk`
+--
+
+CREATE TABLE `barang_masuk` (
+  `id_barang_masuk` char(16) NOT NULL,
+  `supplier_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `barang_id` char(7) NOT NULL,
+  `jumlah_masuk` int(11) NOT NULL,
+  `tanggal_masuk` date NOT NULL,
+  `status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data untuk tabel `barang_masuk`
+--
+
+INSERT INTO `barang_masuk` (`id_barang_masuk`, `supplier_id`, `user_id`, `barang_id`, `jumlah_masuk`, `tanggal_masuk`, `status`) VALUES
+('T-BM-21060900001', 2, 1, 'B000001', 10, '2021-06-09', 1),
+('T-BM-21060900002', 3, 1, 'B000002', 10, '2021-06-09', 1),
+('T-BM-21060900003', 3, 15, 'B000002', 10, '2021-06-09', 1),
+('T-BM-21060900004', 3, 15, 'B000000', 5, '2021-06-09', 1);
+
+--
+-- Trigger `barang_masuk`
+--
+DELIMITER $$
+CREATE TRIGGER `update_stok_masuk` AFTER UPDATE ON `barang_masuk` FOR EACH ROW BEGIN
+      IF (NEW.status = 1) THEN
+      	UPDATE `barang` SET `barang`.`stok` = `barang`.`stok` + NEW.jumlah_masuk WHERE `barang`.`id_barang` = NEW.barang_id;
+      END IF;
+    END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `jenis`
+--
+
+CREATE TABLE `jenis` (
+  `id_jenis` int(11) NOT NULL,
+  `nama_jenis` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data untuk tabel `jenis`
+--
+
+INSERT INTO `jenis` (`id_jenis`, `nama_jenis`) VALUES
+(10, 'Semen'),
+(11, 'Cat'),
+(12, 'Pompa Air');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `satuan`
+--
+
+CREATE TABLE `satuan` (
+  `id_satuan` int(11) NOT NULL,
+  `nama_satuan` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data untuk tabel `satuan`
+--
+
+INSERT INTO `satuan` (`id_satuan`, `nama_satuan`) VALUES
+(2, 'Karung'),
+(6, 'Kaleng'),
+(8, 'Unit');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `sig`
+--
+
+CREATE TABLE `sig` (
+  `id` int(11) NOT NULL,
+  `img` varchar(100) NOT NULL,
+  `USER_ID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `supplier`
+--
+
+CREATE TABLE `supplier` (
+  `id_supplier` int(11) NOT NULL,
+  `nama_supplier` varchar(50) NOT NULL,
+  `no_telp` varchar(15) NOT NULL,
+  `alamat` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data untuk tabel `supplier`
+--
+
+INSERT INTO `supplier` (`id_supplier`, `nama_supplier`, `no_telp`, `alamat`) VALUES
+(1, 'PT. Bangkrut Jaya', '+6221808270005', 'Kabupuaten Dairi, Sidikalang'),
+(2, 'PT. Mandiri USU', '+6221806377044', 'Jl. Besar sidamanik, simpang bahbutong'),
+(3, 'PT. Hancur Lebur', '+6285338885401', 'Balige, Kabupaten Tobasa');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user`
+--
+
+CREATE TABLE `user` (
+  `id_user` int(11) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `no_telp` varchar(15) NOT NULL,
+  `role` enum('gudang','admin','manager') NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `created_at` int(11) NOT NULL,
+  `foto` text NOT NULL,
+  `is_active` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data untuk tabel `user`
+--
+
+INSERT INTO `user` (`id_user`, `nama`, `username`, `email`, `no_telp`, `role`, `password`, `created_at`, `foto`, `is_active`) VALUES
+(1, 'Adminisitrator', 'admin', 'admin@admin.com', '025123456789', 'admin', '$2y$10$xxC9BivHzZ.VT/1zmmUL/uDLquOpOWeLMvgeDuYluxB2LXf9J8WDK', 1568689561, 'user.png', 1),
+(14, 'Kristina Nainggolan', 'kristina', 'kristina@gmail.com', '081923465748', 'manager', '$2y$10$4ssdaKz7yNNp4QpqghY2N.PcRYUA1sEMJMEnRlXIYAUvnd0lbbyHG', 1622253734, 'user.png', 1),
+(15, 'dinda', 'dinda', 'dinda@gmail.com', '12345678', 'gudang', '$2y$10$WE9xwY/Az/9F94rAlUZOuufMuYUkUI3nfa0x4E0Xx/VBuZuJYaqeO', 1623042876, 'user.png', 1),
+(16, 'riris', 'riris', 'riris@gmail.com', '123456', 'manager', '$2y$10$5qm.bNOESU4YXz7tYEa1zuU7TRletWm05c7dO5MvyEvGMXh1xf66a', 1623043000, 'user.png', 1),
+(17, 'jessica', 'jessica', 'jessica@gmail.com', '+6285338885456', 'gudang', '$2y$10$tSisQmdf7B82AZbCIvIDV.cVyR8fU34gU/KT/FZXwhYDgM2SgHY5e', 1623252513, 'user.png', 0),
+(18, 'uli', 'uli', 'uli@gmail.com', '+6285338885493', 'manager', '$2y$10$U6ztMX0y2hjjQQs91gH0DOl5JwkZudqiza8efAlapMwRhkEXF.bEq', 1623252547, 'user.png', 0);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `barang`
+--
+ALTER TABLE `barang`
+  ADD PRIMARY KEY (`id_barang`),
+  ADD KEY `satuan_id` (`satuan_id`),
+  ADD KEY `kategori_id` (`jenis_id`);
+
+--
+-- Indexes for table `barang_keluar`
+--
+ALTER TABLE `barang_keluar`
+  ADD PRIMARY KEY (`id_barang_keluar`),
+  ADD KEY `id_user` (`user_id`),
+  ADD KEY `barang_id` (`barang_id`);
+
+--
+-- Indexes for table `barang_masuk`
+--
+ALTER TABLE `barang_masuk`
+  ADD PRIMARY KEY (`id_barang_masuk`),
+  ADD KEY `id_user` (`user_id`),
+  ADD KEY `supplier_id` (`supplier_id`),
+  ADD KEY `barang_id` (`barang_id`);
+
+--
+-- Indexes for table `jenis`
+--
+ALTER TABLE `jenis`
+  ADD PRIMARY KEY (`id_jenis`);
+
+--
+-- Indexes for table `satuan`
+--
+ALTER TABLE `satuan`
+  ADD PRIMARY KEY (`id_satuan`);
+
+--
+-- Indexes for table `sig`
+--
+ALTER TABLE `sig`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `supplier`
+--
+ALTER TABLE `supplier`
+  ADD PRIMARY KEY (`id_supplier`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `jenis`
+--
+ALTER TABLE `jenis`
+  MODIFY `id_jenis` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+--
+-- AUTO_INCREMENT for table `satuan`
+--
+ALTER TABLE `satuan`
+  MODIFY `id_satuan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+--
+-- AUTO_INCREMENT for table `sig`
+--
+ALTER TABLE `sig`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `supplier`
+--
+ALTER TABLE `supplier`
+  MODIFY `id_supplier` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `barang`
+--
+ALTER TABLE `barang`
+  ADD CONSTRAINT `barang_ibfk_1` FOREIGN KEY (`satuan_id`) REFERENCES `satuan` (`id_satuan`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `barang_ibfk_2` FOREIGN KEY (`jenis_id`) REFERENCES `jenis` (`id_jenis`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `barang_keluar`
+--
+ALTER TABLE `barang_keluar`
+  ADD CONSTRAINT `barang_keluar_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `barang_keluar_ibfk_2` FOREIGN KEY (`barang_id`) REFERENCES `barang` (`id_barang`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `barang_masuk`
+--
+ALTER TABLE `barang_masuk`
+  ADD CONSTRAINT `barang_masuk_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `barang_masuk_ibfk_2` FOREIGN KEY (`supplier_id`) REFERENCES `supplier` (`id_supplier`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `barang_masuk_ibfk_3` FOREIGN KEY (`barang_id`) REFERENCES `barang` (`id_barang`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
